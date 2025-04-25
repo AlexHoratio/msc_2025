2a3a9ab73e939683031bd75b4b50dfc5f393ca11
Alex <horatioesf@protonmail.com>
2025-04-25 13:19:47 +0100

    dereplicate "specific"

--- python/generate_tsv_from_busco_summaries.py
+++ python/generate_tsv_from_busco_summaries.py
@@ -13,6 +13,7 @@ def generate_tsv_from_busco_summaries(busco_summaries_folder):
 
 		busco_data_entry["SampleName"] = re.findall("[R]+[0-9]+", json_data["parameters"]["out"])[0]
 		busco_data_entry["Bin"] = re.findall("(?<=_000)[0-9]+", json_data["parameters"]["out"])[0]
+		busco_data_entry["Specific"] = "1" if ("specific" in filename) else "0"
 
 		busco_data_entry["LineageDataset"] = json_data["lineage_dataset"]["name"]
 		busco_data_entry["LineageBUSCOs"] = json_data["lineage_dataset"]["number_of_buscos"]
@@ -35,9 +36,29 @@ def generate_tsv_from_busco_summaries(busco_summaries_folder):
 		busco_summary_data.append(busco_data_entry)
 
 
+	entries_to_delete = []
+	for i in range(len(busco_summary_data)):
+		for j in range(len(busco_summary_data)):
+			if busco_summary_data[i]["SampleName"] == busco_summary_data[j]["SampleName"] and busco_summary_data[i]["Bin"] == busco_summary_data[j]["Bin"]:
+				print("Match: ", busco_summary_data[i]["Specific"], i, busco_summary_data[i]["Bin"], busco_summary_data[j]["Specific"], j,  busco_summary_data[j]["Bin"])
+				if busco_summary_data[i]["Specific"] != busco_summary_data[j]["Specific"]:
+					if busco_summary_data[i]["Specific"] == "0" and busco_summary_data[j]["Specific"] == "1":
+						entries_to_delete.append(i)
+					elif busco_summary_data[i]["Specific"] == "1" and busco_summary_data[j]["Specific"] == "0":
+						entries_to_delete.append(j)
+				else:
+					if busco_summary_data[i] != busco_summary_data[j]:
+						print("What!")
+						print(busco_summary_data[i])
+						print(busco_summary_data[j])
+						print("========")
+
+	print([busco_summary_data[idx]["Bin"] for idx in entries_to_delete])
+	busco_summary_data = [busco_summary_data[idx] for idx in range(len(busco_summary_data)) if not(idx in entries_to_delete)]
+
 	busco_summary_tsv = ""
 
-	tsv_colnames = ["SampleName", "Bin", "LineageDataset", "LineageBUSCOs", "LineageSpecies", "Complete", "Single Copy", "Multiple", "Fragmented", "Missing", "Number of Markers", "Domain", "Scaffolds", "Contigs", "Total Length", "Gaps", "Scaffold N50", "Contig N50"]
+	tsv_colnames = ["SampleName", "Bin", "Specific", "LineageDataset", "LineageBUSCOs", "LineageSpecies", "Complete", "Single Copy", "Multiple", "Fragmented", "Missing", "Number of Markers", "Domain", "Scaffolds", "Contigs", "Total Length", "Gaps", "Scaffold N50", "Contig N50"]
 
 	for i in range(len(tsv_colnames)):
 		busco_summary_tsv += tsv_colnames[i] + ("\t" if i < len(tsv_colnames) - 1 else "")
